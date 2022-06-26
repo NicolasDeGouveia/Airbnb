567c47b1013108f97fa54aa6bcc48122614235f7
unknown <nicolas.degouveia@gmail.com>
2022-06-26 18:56:12 +0200

    adjusting padding main section

--- src/Main/index.jsx
+++ src/Main/index.jsx
@@ -4,7 +4,7 @@ export default function Main({ card }) {
       <div>
         <p className="mb-6">Classement par ordre de pertinence</p>
       </div>
-      <div className="w-full h-max-[900px] mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-3">
+      <div className="w-full h-max-[900px] mx-auto pt-6 pb-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-3">
         {card}
       </div>
     </div>
